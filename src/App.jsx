de8847aba06379d9a354446570e5502827ea0604
Flacko <lesliejoe2k2@gmail.com>
2024-06-17 14:19:44 +0000

    new feat: add job component

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ import Home from './pages/Home'
 import JobsPages from './pages/JobsPages'
 import NotFound from './pages/NotFound'
 import JobPage from './pages/JobPage'
+import AddJob from './pages/AddJob'
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>} >
         <Route index element={<Home />}/>
         <Route path='/jobs' element={<JobsPages/>}/>
+        <Route path='/add-job' element={<AddJob/>}/>
         <Route path='/jobs/:id' element={<JobPage/>}/>
         <Route path='*' element={<NotFound/>}/>
       </ Route>
