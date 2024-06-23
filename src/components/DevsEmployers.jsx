91bd7f751b6e9531569d7e551211987bc5803bbf
Flacko <lesliejoe2k2@gmail.com>
2024-06-23 15:57:24 +0000

    new feat: routed cta links

--- src/components/DevsEmployers.jsx
+++ src/components/DevsEmployers.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Card from "./Card"
 
 const DevsEmployers = () => {
@@ -11,24 +12,24 @@ const DevsEmployers = () => {
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
-            <a
-              // href="/jobs.html"
+            <Link
+              to={'/jobs'}
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </Card>
           <Card bg="bg-indigo-100">
-          <h2 className="text-2xl font-bold">For Developers</h2>
+          <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            <a
-              // href="/jobs.html"
+            <Link
+              to={'/add-job'}
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Add Job
-            </a>
+            </Link>
           </Card>
         </div>
       </div>
